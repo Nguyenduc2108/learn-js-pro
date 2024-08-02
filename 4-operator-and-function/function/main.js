a80f8f72347cbd35e3c1b04818927b4100765490
Nguyenduc2108 <vanducnguyen2108@gmail.com>
2024-08-02 08:39:24 +0700

    function introduction

---
+++ 4-operator-and-function/function/main.js
@@ -0,0 +1,77 @@
+// 1.Định nghĩa hàm (Function definition)
+function functionName() {
+    // code block
+}
+
+function showGreeting() {
+    console.log("Xin chào!");
+    console.log("Đây là khóa học JavaScript Pro!");
+}
+
+// 2.Gọi hàm (Function call)
+showGreeting(); // In ra: Xin chào!
+// Đây là khóa học JavaScript Pro!
+
+showGreeting(); // In ra: Xin chào!
+// Đây là khóa học JavaScript Pro!
+
+showGreeting(); // In ra: Xin chào!
+// Đây là khóa học JavaScript Pro!
+
+// 3.Hàm có tham số (Parameters)
+function showGreeting(message) {
+    console.log("Xin chào!");
+    console.log(message);
+}
+
+showGreeting("Đây là khóa học của F8!"); // In ra: Xin chào!
+// Đây là khóa học của F8!
+
+showGreeting("Chào mừng bạn đã gia nhập F8!"); // In ra: Xin chào!
+// Chào mừng bạn đã gia nhập F8!
+
+// Mỗi tham số ngăn cách bằng một dấu phẩy (,)
+function demo(param1, param2, param3) {
+    console.log(param1, param2, param3);
+}
+
+demo("Value 1", "Value 2", "Value 3"); // In ra: Value 1, Value 2, Value 3
+demo("Value 1", "Value 2"); // In ra: Value 1, Value 2, undefined
+
+// 4.Tham số mặc định (Default parameters)
+function demo(param1, param2, param3 = "Default value 3") {
+    console.log(param1, param2, param3);
+}
+
+demo("Value 1", "Value 2"); // In ra: Value 1, Value 2, Default value 3
+demo("Value 1", "Value 2", undefined); // In ra: Value 1, Value 2, Default value 3
+demo("Value 1", "Value 2", "Value 3"); // In ra: Value 1, Value 2, Value 3
+
+// 5.Hàm có giá trị trả về (Return value)
+function sum(a, b) {
+    console.log(a + b);
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(2, 3)); // In ra: 5
+const result = sum(2, 3);
+console.log(result); // In ra: 5
+
+const result1 = sum(3, 3);
+console.log(result1); // In ra: 6
+
+const result2 = sum(5, 3);
+console.log(result2); // In ra: 8
+
+function sum(a, b) {
+    a + b;
+}
+console.log(sum(1, 2)); // In ra: undefined
+
+function sum(a, b) {
+    console.log(a + b);
+}
+sum(2, 5); // In ra: 5
--- 4-operator-and-function/operator-and-function.md
+++ 4-operator-and-function/operator-and-function.md
@@ -31,3 +31,41 @@
         . Quên phép chia lấy số dư: phép chia lấy dư sẽ trả về phần dư của phép chia sau khi lấy phần nguyên của kết quả
 
 # 2.Function
+
+    2.1. Hàm là gì?
+        . là một khối code được thiết kế để thực hiện một tác vụ cụ thể
+        . tái sử dụng code
+        .  định nghĩa hàm một lần và gọi hàm đó khi cần
+
+    2.2. Định nghĩa hàm (Function definition):
+        . từ khóa function
+        . sau đó là tên của hàm
+        . tiếp theo là một cặp dấu ngoặc đơn ()
+        . và một cặp dấu ngoặc nhọn {} bao quanh phần thân của hàm
+
+    2.3. Gọi hàm (Calling a function):
+        . sử dụng () sau tên một hàm
+        . có thể gọi hàm nhiều lần
+
+    2.4. Hàm có tham số (Parameters):
+        . định nghĩa tham số
+        . giá trị được truyền vào khi gọi hàm được gọi là đối số
+        . có thể có nhiều tham số
+        . có thể truyền nhiều đối số tương ứng
+        . có thể gọi hàm với số lượng đối số không giống với số lượng tham số (vì không được truyền đối số tương ứng sẽ ra undefined)
+
+    2.5. Tham số mặc định (Default parameters):
+        . giá trị mặc định của tham số sẽ được sử dụng khi đối số tương ứng không được truyền giá trị, hoặc khi truyền giá trị là undefined
+
+    2.6. Hàm trả về giá trị (Return value)
+        . từ khóa return được sử dụng để trả về kết quả khi gọi một hàm
+        . từ khóa return sẽ trả về giá trị và thoát hàm. Khi return được thực thi, những dòng code trong hàm nằm phía sau return sẽ bị bỏ qua
+        . có thể sử dụng lại hàm nhiều lần với các đối số khác nhau
+        . một hàm không return thì sẽ nhận lại undefined khi gọi hàm
+
+    2.7. Lợi ích khi sử dụng hàm
+        . tránh lặp code
+        . phân chia vấn đề lớn thành các phần nhỏ
+        . lỗi thường gặp
+
+    ==> 1 function chỉ nên làm 1 việc
